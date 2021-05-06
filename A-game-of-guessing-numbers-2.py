3c515b0b147ea22563e5c2239ad75f6e55ec9240
maksblink <blinkmaks777@gmail.com>
2021-05-06 13:15:28 +0200

    fin

--- A-game-of-guessing-numbers-2.py
+++ A-game-of-guessing-numbers-2.py
@@ -2,6 +2,9 @@ print("Make up a number from 0 to 1000 and I'll guess it in 10 moves max.")
 max = 1001
 min = -1
 while True:
+    """Main function for program."""
+    guess = int((max - min) / 2) + min
+    print("I guess: " + str(guess))
     """Get proper value provided by user.
 
     Try until user give proper number.
@@ -10,9 +13,6 @@ while True:
     :value provided by user from ["1" = "to big", "2" = "to small", "3" = "you win"]
     """
     ans = input("1 - too big\n2 - too small\n3 - you win\n")
-    """Main function for program."""
-    guess = int((max - min) / 2) + min
-    print("I guess: " + str(guess))
     if (ans != "1") and (ans != "2") and (ans != "3"):
         print("Invalid input.")
     if ans == "3":
